7d3368e0feb87845b2d769c1e99416112a050269
MDN <mark.denardo@gmail.com>
2023-06-17 15:13:18 -0400

    Update lc-00.js

--- lc-00.js
+++ lc-00.js
@@ -17,3 +17,21 @@ shape()
 	//.modulateScrollY(osc(1,1,1))
 	.out(o0)
 render(o0)
+
+//uncomment after lc set to finish
+// const f =(()=>a.fft[0])
+// const arr = []
+// for(i=0;i<10;i++){arr.push(i)}
+// const oscFunc = (o) => (osc(arr.smooth()).kaleid(arr)
+//   .scale(0.1,1)
+//   .repeat(()=>time,1)
+//   .add(noise(arr.smooth())
+//   .rotate(()=>time,0.1)).out(o))
+// const flt=(i,o,m)=>(src(i).modulatePixelate(m).out(o))
+// const flt1=(i,o,m)=>(src(i).modulate(m).kaleid().out(o))
+
+// oscFunc(o0)
+// flt(o0,o1,o1)
+// flt(o1,o2,o0)
+// flt1(o0,o3,o2)
+// render(o3)
