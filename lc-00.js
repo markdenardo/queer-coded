80e4069ba6792d50e6f444fadb00befbcc0076c4
MDN <mark.denardo@gmail.com>
2023-06-17 14:39:56 -0400

    Create lc-00.js

---
+++ lc-00.js
@@ -0,0 +1,19 @@
+//welcome to queer coded 🏳️‍⚧️ 🌈 💕
+//we're MDN (they/them)
+//we're going to try and livecode some hydra from nothing. :)_
+const pi = () => Math.random(Math.PI)
+const t = () => time
+const f = () => a.fft[0]
+//const osc1 = () => (osc(1, 1, 1))
+shape()
+	//.rotate(t, 1)
+.scale(pi)
+ 	.pixelate()
+ 	.repeat(t, t)
+ .kaleid(pi,f)
+ 	//.modulate(o0,f)
+ 	.modulate(o0, t)
+ 	.rotate(t, f)
+	//.modulateScrollY(osc(1,1,1))
+	.out(o0)
+render(o0)
