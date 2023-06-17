50de46ccd40ff8b21fcbdb18e83069c8be680db1
MDN <mark.denardo@gmail.com>
2023-06-17 15:53:45 -0400

    Update frac-and-ide.js

--- frac-and-ide.js
+++ frac-and-ide.js
@@ -62,18 +62,16 @@ feedbacks(BRANCHES)
 	// 		.color(1, 0, 10)
 	// 		.modulate(o0))
 	.out(o0)
+render(o0)
 
 //ideon
 // by MDN @markdenardo
 
-// s1.initVideo("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmZkNmFmMWJlZTM1NjVmODc5NjhlMGIxMGI3N2NhMjQ0NmRmNTc2NyZjdD1n/DkleR7V8RMtrim0cH5/giphy.mp4")
-// s2.initVideo("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzEyZDUyODk0YWYwYzc2ZjExZDQ1ZjQ5OGM5MjAzODFmZGI0MThmYiZjdD1n/fvCoIEB8YtKghURZcA/giphy.mp4")
-// src(s1)
-// 	.pixelate(100, 100)
-// 	.add(s1, f)
-// 	.add(s2, f)
-// 	.out(o1)
-
- render(o0)
-//render(o1)
-
+s1.initVideo("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmZkNmFmMWJlZTM1NjVmODc5NjhlMGIxMGI3N2NhMjQ0NmRmNTc2NyZjdD1n/DkleR7V8RMtrim0cH5/giphy.mp4")
+s2.initVideo("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzEyZDUyODk0YWYwYzc2ZjExZDQ1ZjQ5OGM5MjAzODFmZGI0MThmYiZjdD1n/fvCoIEB8YtKghURZcA/giphy.mp4")
+src(s1)
+	.pixelate(100, 100)
+	.add(s1, f)
+	.add(s2, f)
+	.out(o1)
+render(o1)
